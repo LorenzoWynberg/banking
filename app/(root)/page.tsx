79a053fca2af0c637653b17373adbdcf0629df24
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-05-20 11:54:16 -0600

    Initial Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Lorenzo"}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your acount and transactions efficiently."
+          />
+        </header>
+        <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={50304.63}
+        />
+      </div>
+    </section>
   )
 }
 
