2adda56c72fc8332c44ff8d2cf43eb2b62e0bcd9
Lorenzo Wynberg <dev.wynberg@gmail.com>
2024-06-08 04:11:03 -0600

    Add RightSideBar Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,21 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Lorenzo' };
+  const loggedIn = {
+    firstName: 'Lorenzo',
+    lastName: 'Wynberg',
+    email: 'contact@ozner.com',
+  };
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox
             type='greeting'
-            title='welcome'
+            title='Welcome'
             user={loggedIn?.firstName || 'Guest'}
             subtext='Access and manage your acount and transactions efficiently.'
           />
@@ -21,6 +26,11 @@ const Home = () => {
           />
         </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 20300 }, { currentBalance: 30000.63 }]}
+      />
     </section>
   );
 };
